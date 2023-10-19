ec80f43252c714addc1dcf890b8f73189c28e2fc
Mohamed Mafaz <55541056+Mafaz03@users.noreply.github.com>
2023-10-19 09:36:51 +0530

    Update Exporting_Script.py
    Installing pandas for blender environment.

--- Exporting_Script.py
+++ Exporting_Script.py
@@ -1,4 +1,33 @@
+import sys
+import subprocess
+import os
 import bpy
+
+# Installing pandas
+def python_exec():
+    import os
+    import bpy
+    try:
+        # 2.92 and older
+        path = bpy.app.binary_path_python
+    except AttributeError:
+        # 2.93 and later
+        import sys
+        path = sys.executable
+    return os.path.abspath(path)
+
+
+try:
+    from pandas import read_csv
+except:
+    
+ 
+    python_exe = python_exec()
+   # upgrade pip
+    subprocess.call([python_exe, "-m", "ensurepip"])
+    subprocess.call([python_exe, "-m", "pip", "install", "--upgrade", "pip"])
+   # install required packages
+    subprocess.call([python_exe, "-m", "pip", "install", "pandas"])
 
 # Get references to the objects
 object1 = bpy.data.objects.get("Object1")
